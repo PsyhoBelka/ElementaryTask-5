b74c0189dd23add5cc15b3bfc9a9ea8a08d1348c
PsyhoBelka <agroghkov@gmail.com>
2018-08-02 15:45:39 +0300

    Add parametrized tests

--- tests/ua/rozhkov/NumberParserTest.java
+++ tests/ua/rozhkov/NumberParserTest.java
@@ -1,22 +1,46 @@
 package ua.rozhkov;
 
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Assertions;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class NumberParserTest {
+@RunWith(Parameterized.class)
+public class NumberParserTest {
+	String actual;
+	String expected;
+	NumberParser numberParser;
+	
+	public NumberParserTest (String actual, String expected) {
+		this.actual = actual;
+		this.expected = expected;
+		this.numberParser = new NumberParser();
+	}
+	
+	@Parameterized.Parameters
+	public static Collection<Object[]> data () {
+		return Arrays.asList(new Object[][]{
+				{"22", "двадцать два"},
+				{"459", "четыреста пятьдесят девять"}
+		});
+	}
+	
+	@Ignore
+	@org.junit.Before
+	public void setUp () throws Exception {
+	}
 	
 	@Test
-	void parseToDigits () {
-	}
-	
-	@org.junit.jupiter.api.Test
-	void parseToRanks () {
-	}
-	
-	@org.junit.jupiter.api.Test
-	void parseRank () {
+	public void parseNumber () throws Exception {
+		String ac = numberParser.parseNumber(actual);
+		assertEquals(ac, expected);
 	}
 	
 }
